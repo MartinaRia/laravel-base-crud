ced95ec3b71fea0e4e88ba551d333f88542ca764
Martina <martina.ria@hotmail.com>
2020-06-21 19:31:44 +0200

    Store

--- crud-project/app/Http/Controllers/OminiController.php
+++ crud-project/app/Http/Controllers/OminiController.php
@@ -7,25 +7,43 @@ use Illuminate\Http\Request;
 
 class OminiController extends Controller
 {
-  public function index(){
 
+  public function index(){
     $ominiAll = OminiModel::all();
 
     return view('home', compact('ominiAll'));
   }
 
-  public function show($id){
 
+  public function show($id){
     $omino = OminiModel::findOrFail($id);
 
     return view('ominidetails', compact('omino'));
   }
 
-  public function destroy($id){
 
+  public function destroy($id){
     $omino = OminiModel::findOrFail($id);
     $omino -> delete();
 
     return redirect() -> route('home');
+  }
+
+
+  public function store(Request $request){
+    $data = $request -> all();
+    $omino = new OminiModel;
+
+    $omino -> firstname = $data['firstname'];
+    $omino -> lastname = $data['lastname'];
+    $omino -> address = $data['address'];
+    $omino -> code = $data['code'];
+    $omino -> state = $data['state'];
+    $omino -> phonenumber = $data['phonenumber'];
+    $omino -> role = $data['role'];
+
+    $omino ->save();
+
+    return redirect() -> route ('home');
   }
 }
--- crud-project/routes/web.php
+++ crud-project/routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'OminiController@index')->name('home');
 Route::get('/ominidetails/{id}', 'OminiController@show')->name('show');
 Route::get('/ominidetails/delete/{id}', 'OminiController@destroy')->name('destroy');
+Route::post('/store', 'OminiController@store')->name('store');
